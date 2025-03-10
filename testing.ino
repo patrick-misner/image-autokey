18d77d1b2781ca4d141b597369da9538b2e1c32d
Patrick Misner <misner.patrick@gmail.com>
2025-03-10 02:06:07 -0600

    Update testing.ino

--- testing.ino
+++ testing.ino
@@ -17,53 +17,119 @@
 #define P34 34  // Define P34 pin
 
 // Variables
-const char *imgPw = "imagePWORD";
-const char *winUser = "USERNAME";
-const char *winPw = "PASSWORD";
+#define IMG_PW "imagePWORD"
+#define WIN_USR = "USERNAME";
+#define WIN_PW = "PASSWORD";
+
+// Model 1 parameters
+const char* model1BootKey = "KEY_F12";
+unsigned long model1BootDelay = 5000;
+int model1DownArrow = 1;
+unsigned long model1LoginDelay = 50000;
+
+// Model 2 parameters
+const char* model2BootKey = "KEY_F12";
+unsigned long model2BootDelay = 5000;
+int model2DownArrow = 2;
+unsigned long model2LoginDelay = 50000;
+
+// Model 3 parameters
+const char* model3BootKey = "KEY_F9";
+unsigned long model3BootDelay = 5000;
+int model3DownArrow = 2;
+unsigned long model3LoginDelay = 50000;
+
+// Model 4 parameters
+const char* model4BootKey = "KEY_F9";
+unsigned long model4BootDelay = 5000;
+int model4DownArrow = 2;
+unsigned long model4LoginDelay = 50000;
+
+// Model 5 parameters
+const char* model5BootKey = "KEY_F9";
+unsigned long model5BootDelay = 5000;
+int model5DownArrow = 2;
+unsigned long model5LoginDelay = 50000;
+
+// Pointers to hold the selected model parameters
+const char* bootKey;
+unsigned long bootDelay;
+int downArrow;
+unsigned long loginDelay;
+
+
+// WinPE Option 1 parameters
+const char* option1Step1 = IMG_PW;
+const char* option1Step2 = "2";
+const char* option1Step3 = "";
+
+// WinPE Option 2 parameters
+const char* option2Step1 = "2";
+const char* option2Step2 = IMG_PW;
+const char* option2Step3 = "2";
+
+// WinPE Option 2 parameters
+const char* option3Step1 = "2";
+const char* option3Step2 = IMG_PW;
+const char* option3Step3 = "2";
+
+// Pointers to hold the selected WinPE Option parameters
+const char* optionStep1;
+const char* optionStep2;
+const char* optionStep3;
+
 
 // Function to type a string
 void typeString(const char *str) {
     for (int i = 0; str[i] != '\0'; i++) {
         Keyboard_write(str[i]);
     }
+    delay(500);
 }
 
-// Function to press key and delay
+// Function to Key press and Delay
 void typeKey(uint8_t key) {
     Keyboard_press(key);
-    delay(25);  // Short delay to simulate a real key press
+    delay(25);
     Keyboard_release(key);
     delay(475);
 }
 
-// Function to Type Character and delay
+// Function to type a single character (Maybe not needed?)
 void typeCharacter(char c) {
     Keyboard_write(c);  // Writes the character as if typed on a keyboard
     delay(500);  // Short delay to simulate natural typing
 }
 
-// Check if P32 and P14 are shorted
-bool checkIfOpt1Shorted() {
+void checkModel1() {
     digitalWrite(P32, LOW);
     delay(50);
     bool opt1IsShorted = digitalRead(P14) == LOW;
     digitalWrite(P32, HIGH);
     delay(50);
-    return opt1IsShorted; 
+    if (opt1IsShorted) {
+        bootKey = model1BootKey;
+        bootDelay = model1BootDelay;
+        downArrow = model1DownArrow;
+        loginDelay = model1LoginDelay;
+    }
 }
 
-// Check if P14 and P15 are shorted P14 reads high on boot
-bool checkIfOpt2Shorted() {
+void checkModel2() {
     digitalWrite(P15, LOW);
     delay(50);
     bool opt2IsShorted = digitalRead(P14) == LOW;
     digitalWrite(P15, HIGH);
     delay(50);
-    return opt2IsShorted;
+    if (opt2IsShorted) {
+        bootKey = model2BootKey;
+        bootDelay = model2BootDelay;
+        downArrow = model2DownArrow;
+        loginDelay = model2LoginDelay;
+    }
 }
 
-// Check if P15 and P16 are shorted P16 reads LOW on boot
-bool checkIfOpt3Shorted() {
+void checkModel3() {
     delay(50);
     digitalWrite(P17, LOW);
     delay(50);
@@ -71,15 +137,65 @@ bool checkIfOpt3Shorted() {
     digitalWrite(P15, LOW);
     digitalWrite(P17, HIGH);
     delay(50);
-    return opt3IsShorted;
+    if (opt3IsShorted) {
+        bootKey = model3BootKey;
+        bootDelay = model3BootDelay;
+        downArrow = model3DownArrow;
+        loginDelay = model3LoginDelay;
+    }
 }
 
-bool checkIfOpt4Shorted() {
+void checkModel4() {
     delay(50);
     bool opt4IsShorted = digitalRead(P16);
     digitalWrite(P17, LOW);
     delay(50);
-    return opt4IsShorted;
+    if (opt4IsShorted) {
+        bootKey = model4BootKey;
+        bootDelay = model4BootDelay;
+        downArrow = model4DownArrow;
+        loginDelay = model4LoginDelay;
+    }
+}
+
+void checkModel5() {
+    digitalWrite(P30, LOW);
+    delay(50);
+    bool opt5IsShorted = digitalRead(P31) == LOW;
+    digitalWrite(P30, HIGH);
+    delay(50);
+    if (opt5IsShorted) {
+        bootKey = model5BootKey;
+        bootDelay = model5BootDelay;
+        downArrow = model5DownArrow;
+        loginDelay = model5LoginDelay;
+    }
+}
+
+void checkOption1() {
+    digitalWrite(P34, LOW);
+    delay(50);
+    bool opt6IsShorted = digitalRead(P33) == LOW;
+    digitalWrite(P34, HIGH);
+    delay(50);
+    if (opt6IsShorted) {
+        optionStep1 = option1Step1;
+        optionStep2 = option1Step2;
+        optionStep3 = option1Step3;
+    }
+}
+
+void checkOption2() {
+    digitalWrite(P11, LOW);
+    delay(50);
+    bool opt7IsShorted = digitalRead(P33) == LOW;
+    digitalWrite(P11, HIGH);
+    delay(50);
+    if (opt7IsShorted) {
+        optionStep1 = option2Step1;
+        optionStep2 = option2Step2;
+        optionStep3 = option2Step3;
+    }
 }
 
 
@@ -87,6 +203,13 @@ bool checkIfOpt4Shorted() {
 
 void setup() {
     USBInit();
+    checkModel1();
+    checkModel2();
+    checkModel3();
+    checkModel4();
+    checkModel5();
+    checkOption1();
+    checkTopion2();
     pinMode(LED_BUILTIN, OUTPUT);
     digitalWrite(LED_BUILTIN, HIGH);
     pinMode(P32, OUTPUT);
@@ -94,42 +217,22 @@ void setup() {
     pinMode(P15, OUTPUT);
     pinMode(P16, INPUT);   
     pinMode(P17, OUTPUT);
+    pinMode(P30, OUTPUT);
     pinMode(P31, INPUT);
-    pinMode(P32, OUTPUT);
+    pinMode(P11, OUTPUT);
+    pinMode(P33, INPUT);
+    pinMode(P34, OUTPUT);
     delay(5000);  // Initial delay to ensure USB is ready
 
-    // Check if P32 and P14 are shorted
-    bool opt1IsShorted = checkIfOpt1Shorted();
 
-    // Check if P14 and P15 are shorted
-    bool opt2IsShorted = checkIfOpt2Shorted();
+    typeString(optionStep1);
+    typeString(optionStep2);
+    typeString(optionStep3);
+    typeString("I want this to work");
+    typeKey(KEY_RETURN);
+    typeString("I want this and work reliably");
+    typeKey(KEY_RETURN);
 
-    // Check if P15 and P16 are shorted
-    bool opt3IsShorted = checkIfOpt3Shorted();
-
-    // Check if P16 and P17 are shorted
-    bool opt4IsShorted = checkIfOpt4Shorted();
-
-    if (opt1IsShorted) {
-      typeString("OPTION 1");
-    }
-
-    if (opt2IsShorted) {
-      typeString("OPTION 2");
-    }
-
-    if (opt3IsShorted) {
-      typeString("OPTION 3");
-    }
-
-    if (opt4IsShorted) {
-      typeString("OPTION 4");
-    }
-    
-    Keyboard_press(KEY_RETURN);  // HID Code for Enter
-    delay(25);
-    Keyboard_release(KEY_RETURN);
-    typeString(winPw);
 }  // <-- Closing brace for setup()
 
 void loop() {
